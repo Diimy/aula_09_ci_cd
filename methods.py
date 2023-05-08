f9dc9777b4320c2c894ad27a50804f7b604ea767
DIMITRI ZENARO BASTOS DE OLIVEIRA <22.00820-9@maua.br>
2023-05-08 08:47:19 -0300

    adicionado os metodos

--- methods.py
+++ methods.py
@@ -6,3 +6,14 @@ def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
 
+def sums(val1,val2):
+    return val1+val2
+
+def sub(val1,val2):
+    return val1-val2
+
+def divs(val1,val2):
+    return val1/val2
+
+def multp(val1,val2):
+    return val1*val2
